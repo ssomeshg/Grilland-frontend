cd41e1e3cb73e7ffe16627c096636764f86d2af4
ssomeshg <121214623+ssomeshg@users.noreply.github.com>
2024-09-19 20:44:37 +0530

    responsive check

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,7 +5,7 @@ import { FaUserAlt } from "react-icons/fa";
 
 function Header() {
   return (
-    <header className='py-5  pr-5 h-24 bg-white shadow-md absolute w-full lg:pl-72'>
+    <header className='py-5  pr-5 h-24 bg-white shadow-md absolute w-full pl-72'>
         <div className="navlogos flex gap-5 items-center justify-end px-2">
             <div className='text-right'>
                 <p className='text-sm font-medium text-gray-500'>Good Morning !</p>
